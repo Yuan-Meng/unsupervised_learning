7c663573002d5273fccb96c65b71e2d13c734480
Yuan Meng <hail_to_psyche@berkeley.edu>
2020-04-21 18:59:36 -0700

    New analyeses

---
+++ .ipynb_checkpoints/word_2dviz-checkpoint.py
@@ -0,0 +1,99 @@
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import umap
+import matplotlib.pyplot as plt
+
+# PCA
+def pca_plot(model):
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    pca_model = PCA(n_components=2)
+    new_values = pca_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(12, 12))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(
+            labels[i],
+            xy=(x[i], y[i]),
+            xytext=(5, 2),
+            textcoords="offset points",
+            ha="right",
+            va="bottom",
+        )
+    plt.show()
+
+# t-SNE
+def tsne_plot(model):
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    tsne_model = TSNE(
+        perplexity=40, n_components=2, init="pca", n_iter=2500, random_state=23
+    )
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(12, 12))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(
+            labels[i],
+            xy=(x[i], y[i]),
+            xytext=(5, 2),
+            textcoords="offset points",
+            ha="right",
+            va="bottom",
+        )
+    plt.show()
+    
+# UMAP
+def umap_plot(model):
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    umap_model = umap.UMAP(n_neighbors=5, min_dist=0.3, metric="correlation")
+    new_values = umap_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(12, 12))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(
+            labels[i],
+            xy=(x[i], y[i]),
+            xytext=(5, 2),
+            textcoords="offset points",
+            ha="right",
+            va="bottom",
+        )
+    plt.show()
